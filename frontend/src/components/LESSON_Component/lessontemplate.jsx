e975d3813b2c92bd09084ba923af7862daf19393
Savindu gunasekar <savindugunasekara@gmail.com>
2024-03-23 17:21:15 +0530

    added lesson template

---
+++ frontend/src/components/LESSON_Component/lessontemplate.jsx
@@ -0,0 +1,52 @@
+// import React from "react";
+
+// const Lesson = () => {
+//   return (
+//     <div className="container  w-full mx-auto xl:w-[9/10]  mt-3  ">
+//       <div className="  bg-slate-300 mx-3 md:flex xl:w-[9/10]  xl:mx-auto  mb-5 shadow-sm rounded-lg hover:shadow-lg transition-all duration-250 ">
+//         <div className=" w-full md:w-2/5  ">
+//           <img
+//             className="w-full h-[200px] rounded-t-lg md:h-full  object-cover"
+//             src="https://img.freepik.com/premium-photo/background-featuring-engineering-gears-machinery-showcasing-intricate-mechanical-aspects-engineering-generative-ai_155807-17125.jpg"
+//             alt=""
+//           />
+//         </div>
+
+//         <div className=" p-4  w-full md:w-3/5 ">
+//           <div className="text-3xl font-bold my-2 ">Mechanics</div>
+//           <div class="h-[70px]">
+//             <p>Mechanics is the branch of physics that deals with the behavior of
+//             physical bodies when subjected</p>
+//           </div>
+//           <div className="flex mt-3 mb-3 gap-x-5">
+//             <div className="flex items-center">
+//               <i
+//                 class="fa-solid fa-graduation-cap "
+//                 style={{ color: "#c744f2", fontSize: "25px" }}
+//               ></i>
+//               <p className="ml-1 text-base font-bold ">115 students</p>
+//             </div>
+//             <div className="flex items-center">
+//               <i
+//                 class="fa-solid fa-file "
+//                 style={{ color: "#c744f2", fontSize: "25px" }}
+//               ></i>
+//               <p className="ml-2 text-base font-bold">20 courses</p>
+//             </div>
+//           </div>
+
+//           <button class="bg-[#c744f2] hover:bg-[#9b25c2] transform hover:scale-105  text-white font-bold mt-3 transition-all duration-300 py-2 px-4 rounded">
+//             Enroll Now
+//           </button>
+//         </div>
+        
+//       </div>
+      
+
+      
+      
+//     </div>
+//   );
+// };
+
+// export default Lesson;
